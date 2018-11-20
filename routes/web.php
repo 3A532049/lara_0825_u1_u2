ee5905a5c8dbe6d7870a49323ab830abef6f69bd
3a532049 <shelly213334@gmail.com>
2018-11-20 15:26:51 +0800

    更新資料-換用 save方法

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,16 @@ Route::get('/', function () {
     //$posts=\App\Post::where('id','<',10)‐>orderBy('id','DESC')‐>get();
     //dd($posts);
 
+    //$post=\App\Post::find(1);
+   //$post->update([
+        //'title'=>'updatedtitle',
+       // 'content'=>'updatedcontent',
+   // ]);
+
     $post=\App\Post::find(1);
-    $post->update([
-        'title'=>'updatedtitle',
-        'content'=>'updatedcontent',
-    ]);
+    $post->title='savedtitle';
+    $post->content='savedcontent';
+    $post->save();
+
 
 });
